18e319f84ed532eefcbe262ddcd15031d0bee02e
wuhunfeng <wuhunfeng@163.com>
2025-06-10 20:57:08 +0800

    Update site.config.js

--- config/site.config.js
+++ config/site.config.js
@@ -14,7 +14,7 @@ module.exports = {
   icon: '/icons/128.png',
 
   // The name of your website. Present alongside your icon.
-  title: "ChenFeng's OneDrive",
+  title: "ekao.app.tc",
 
   // The folder that you are to share publicly with onedrive-cf-index-ng. Use '/' if you want to share your root folder.
   baseDirectory: process.env.BASE_DIRECTORY || '/Art',
